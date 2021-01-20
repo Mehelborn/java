699adaea407ed1089e33da78d1b064d520cb5e88
Mehelborn <33414237+Mehelborn@users.noreply.github.com>
2021-01-20 12:34:08 +0700

    20.01.2020

--- example.java
+++ example.java
@@ -1,5 +1,23 @@
 public class example {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int beernum = 99;
+        String word = "botles (botle)";
+
+        while (beernum > 0) {
+            if(beernum == 1) {
+                word = "botle";
+            }
+            System.out.println(beernum + " " + word + " of beer on wall.");
+            System.out.println(beernum + " " + word + " of beer.");
+            System.out.println("Take a botle.");
+            System.out.println("Let go in the circle.");
+            beernum = beernum - 1;
+            if (beernum > 0) {
+                System.out.println(beernum + " " + word + " of beer on wall.");
+            } else {
+                System.out.println("No more beer on wall");
+            }
+        }
+
     }
 }
